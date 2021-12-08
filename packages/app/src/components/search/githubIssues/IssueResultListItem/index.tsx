8d45a9fa5b009e8e336e938e11b450ba707c4c14
Anatoly <anatoly.pashias6@gmail.com>
2021-12-08 19:24:47 +0200

    added state chip

--- packages/app/src/components/search/githubIssues/IssueResultListItem/index.tsx
+++ packages/app/src/components/search/githubIssues/IssueResultListItem/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ListItem, ListItemAvatar, ListItemText, Divider, makeStyles } from '@material-ui/core';
-import { Avatar } from '@material-ui/core';
+import { Avatar, Box, Chip } from '@material-ui/core';
 import { Link } from '@backstage/core-components';
 
 type Props = {
@@ -14,7 +14,14 @@ const useStyles = makeStyles({
     display: "-webkit-box",
     "-webkit-line-clamp": 3,
     "-webkit-box-orient": "vertical"
-  }
+  },
+  flexContainer: {
+    flexWrap: 'wrap',
+  },
+  itemText: {
+    width: '100%',
+    marginBottom: '1rem',
+  },
 })
 
 export const IssueResultListItem = ({ result }: Props) => {
@@ -22,7 +29,7 @@ export const IssueResultListItem = ({ result }: Props) => {
 
   return (
     <Link to={result.location}>
-      <ListItem alignItems="flex-start">
+      <ListItem alignItems="flex-start" className={classes.flexContainer}>
         <ListItemAvatar>
           <Avatar
             src={result.user.avatar_url}
@@ -30,6 +37,7 @@ export const IssueResultListItem = ({ result }: Props) => {
           />
         </ListItemAvatar>
         <ListItemText
+          className={classes.itemText}
           primaryTypographyProps={{ variant: 'h6' }}
           primary={result.title}
           secondary={result.text}
@@ -37,6 +45,16 @@ export const IssueResultListItem = ({ result }: Props) => {
             className: classes.lineClamp
           }}
         />
+        <Box>
+          <Chip
+            label={result.state}
+            size="small"
+            style={{
+              backgroundColor: result.state === "open" ? "#238636" : "#8957e5",
+              color: '#fff'
+            }}
+          />
+        </Box>
       </ListItem>
       <Divider />
     </Link>
